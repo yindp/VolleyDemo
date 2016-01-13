969de307b23093edc3119808e3c4703bb365e92a
Simo YiN <378714297@qq.com>
2016-01-13 19:35:38 +0800

    Initial commit

---
+++ app/src/main/java/yin/colin/rdc/yinom/com/volleydemo/LoginActivity.java
@@ -0,0 +1,13 @@
+package yin.colin.rdc.yinom.com.volleydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
